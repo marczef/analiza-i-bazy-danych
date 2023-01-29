4332adb3581b09a02c3719e2bc0191dcc4942e5a
Marcjanna <marcjanna.bakowska@gmail.com>
2023-01-29 23:30:43 +0100

    update

--- lab14/app.py
+++ lab14/app.py
@@ -12,3 +12,10 @@ def extract_sentiment(text):
 
 def text_contain_word(word: str, text: str):
     return word in text
+
+def bubble_sort(arr):
+    for i in arr(0,len(input)-1):
+        for j in range(0,len(input)-1):
+            if input[j] > input[j+1]:
+                (input[j],input[j+1]) = (input[j+1],input[j])
+    return input
